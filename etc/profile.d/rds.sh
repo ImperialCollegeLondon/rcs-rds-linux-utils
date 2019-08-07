8c6ab81e4335f0ea621d4d01759e494c65aa8075
M J Harvey <m.j.harvey@imperial.ac.uk>
2019-08-07 18:53:52 +0100

    profile.d

---
+++ etc/profile.d/rds.sh
@@ -0,0 +1,5 @@
+export PATH=$PATH:/opt/rcs-rds-linux-utils/bin
+
+export RDS=/rds/general/user/${USER}
+export RDS_PROJECT=/rds/general/project/
+export EPHEMERAL=/rds/general/user/${USER}/ephemeral
